4fd9e707eff5f256870811161c76484b399700f4
dariuscruceru21 <dariuscruceru@gmail.com>
2024-12-06 13:35:11 +0200

    resolved bugs regarding assignment class and databse table

--- CourseProviderDatabase/src/Main.java
+++ CourseProviderDatabase/src/Main.java
@@ -1,10 +1,9 @@
-import Models.Course;
-import Models.Instructor;
-import Models.Student;
-import Models.User;
+import Models.*;
+import Models.Module;
 import Repository.DataBaseRepository;
 
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -20,11 +19,18 @@ public class Main {
 //        // Manually add the ID as part of the object creation
          Instructor student = new Instructor(9, "Paul", "paul123", "paul@gmail.com", "instructor");
          Course course = new Course(2,"ALgebra","usor",10,"2024-07-12","2024-10-11",9);
+         Module module = new Module(1,"Linked Lists","hard content");
+        Assignment assignment = new Assignment(1,"optional assignment", "2024-12-07",100);
 //
 //        // Pass column names including 'userid'
          List<String> columnNames = Arrays.asList("courseID", "courseTitle", "description", "availableSpots", "startDate", "endDate","instructorId");
-        List<String> columnNames2 = Arrays.asList("userid","username","password","email","type");
+         List<String> columnNames2 = Arrays.asList("userid","username","password","email","type");
+         List<String> columnName3 = Arrays.asList("moduleID", "moduleTitle", "moduleContent");
+
+         List<String> columnName4 = Arrays.asList("assignmentID", "description","dueDate","score");
          DataBaseRepository<Course> dataBaseRepository = new DataBaseRepository<>("course", Course.class, columnNames);
+         DataBaseRepository<Module> dataBaseRepository1 = new DataBaseRepository<>("module",Module.class,columnName3);
+         DataBaseRepository<Assignment> dataBaseRepository2 = new DataBaseRepository<>("assignment",Assignment.class,columnName4);
 
 
         //System.out.println("courseId".toLowerCase());
@@ -36,7 +42,10 @@ public class Main {
 //        System.out.println("Print");
 //        System.out.println(System.getenv("DB_PASSWORD"));
 
-        System.out.println(dataBaseRepository.getAll());
+        dataBaseRepository2.create(assignment);
+
+
+
 
     }
 }
