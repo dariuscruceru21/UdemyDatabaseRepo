ba7103d1fe3a808579bf0e439208dc4aaa2d9733
dariuscruceru21 <dariuscruceru@gmail.com>
2025-01-04 16:30:36 +0200

    added choice o f repo type to auth service

--- CourseProviderDatabase/src/Service/AuthenticationService.java
+++ CourseProviderDatabase/src/Service/AuthenticationService.java
@@ -3,38 +3,63 @@ package Service;
 import Models.*;
 import Models.Module;
 import Repository.DataBaseRepository;
+import Repository.FileRepository;
+import Repository.IRepository;
+import Repository.InMemoryRepo;
 import Utils.Utils;
 
 import java.util.List;
 
 public class AuthenticationService {
     Utils utils = new Utils();
-    private DataBaseRepository<Student> studentDataBaseRepository= new DataBaseRepository<>("student",Student.class,utils.getUsersParameters());
-    private DataBaseRepository<Admin> adminDataBaseRepository = new DataBaseRepository<>("admin",Admin.class,utils.getUsersParameters());
-    private DataBaseRepository<Instructor> instructorDataBaseRepository = new DataBaseRepository<>("instructor",Instructor.class,utils.getUsersParameters());
+    private IRepository<Student> studentIRepository;
+    private IRepository<Admin> adminIRepository;
+    private IRepository<Instructor> instructorIRepository;
 
 
 
 
-
-    public AuthenticationService() {}
+    public AuthenticationService(String storageMethode) {
+        switch (storageMethode) {
+            case "inmemory":
+                studentIRepository = new InMemoryRepo<>();
+                adminIRepository = new InMemoryRepo<>();
+                instructorIRepository = new InMemoryRepo<>();
+                break;
+            case "file":
+                studentIRepository = new FileRepository<>("student.csv");
+                adminIRepository = new FileRepository<>("admin.csv");
+                instructorIRepository = new FileRepository<>("instructor.csv");
+                break;
+            case "database":
+                studentIRepository = new DataBaseRepository<>("student",Student.class,utils.getUsersParameters());
+                adminIRepository = new DataBaseRepository<>("admin",Admin.class,utils.getUsersParameters());
+                instructorIRepository = new DataBaseRepository<>("instructor",Instructor.class,utils.getUsersParameters());
+                break;
+            default:
+                studentIRepository = new DataBaseRepository<>("student",Student.class,utils.getUsersParameters());
+                adminIRepository = new DataBaseRepository<>("admin",Admin.class,utils.getUsersParameters());
+                instructorIRepository = new DataBaseRepository<>("instructor",Instructor.class,utils.getUsersParameters());
+                break;
+        }
+    }
 
     public User authenticate(String username, String password) {
-        List<Student>students = studentDataBaseRepository.getAll();
+        List<Student>students = studentIRepository.getAll();
         for(Student student : students) {
             if(student.getUsername().equals(username) && student.getPassword().equals(password)) {
                 return student;
             }
         }
 
-        List<Admin>admins = adminDataBaseRepository.getAll();
+        List<Admin>admins = adminIRepository.getAll();
         for(Admin admin : admins) {
             if(admin.getUsername().equals(username) && admin.getPassword().equals(password)) {
                 return admin;
             }
         }
 
-        List<Instructor>instructors = instructorDataBaseRepository.getAll();
+        List<Instructor>instructors = instructorIRepository.getAll();
         for(Instructor instructor : instructors) {
             if(instructor.getUsername().equals(username) && instructor.getPassword().equals(password)) {
                 return instructor;
